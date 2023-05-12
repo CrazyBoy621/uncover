94b1fc0ac171a399bf1e4ab5e277baa672fc3528
CrazyBoy <65382951+CrazyBoy621@users.noreply.github.com>
2023-05-12 23:21:56 +0500

    Initial Commit

---
+++ uncover/uncoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  uncoverApp.swift
+//  uncover
+//
+//  Created by Shohjahon Rakhmatov on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct uncoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
